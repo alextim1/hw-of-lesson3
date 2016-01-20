19e0792512513957acc3139994163c82018fa6e6
alextim <alexey.timohin@gmail.com>
2016-01-20 13:34:53 +0200

    test does not work. Why?

---
+++ MatrixReader/MatrixReader/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MatrixReader
+{
+    public class Program
+    {
+        public static void MatrixReaderFromFile(string fileName, out int[,] matrix)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            string dimensions = sr.ReadLine();
+            string[] dimensionsOfMatrix = dimensions.Split('x');
+            int Rows = int.Parse(dimensionsOfMatrix[0]);
+            int Columns = int.Parse(dimensionsOfMatrix[1]);
+
+            int[,] outputMatrix = new int[Rows, Columns];
+
+            string nextLine;
+            string[] nextLineArr;
+
+            for (int i=0; i< Rows; i++)
+            {
+               
+                if (sr.EndOfStream)
+                {
+                    for (int j = 0; j<Columns; j++)
+                    {
+                        outputMatrix[i, j] = 0;
+                    }
+                }
+                else
+                {
+                    nextLine = sr.ReadLine();
+                    nextLineArr = nextLine.Split(' ');
+                    for (int j = 0; j < Columns; j++)
+                    {
+                        if (j>nextLineArr.Length-1)
+                        {
+                            outputMatrix[i, j] = 0;
+                        }
+                        else
+                        {
+                            outputMatrix[i, j] = int.Parse(nextLineArr[j]);
+                        }
+                       
+                    }
+                }
+            }
+            matrix = outputMatrix;
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] matrix;
+            MatrixReaderFromFile(@"D:\matrix.txt", out matrix);
+            for (int i=0;i<=matrix.GetUpperBound(0);i++)
+            {
+                for (int j=0;j<=matrix.GetUpperBound(1);j++)
+                {
+                    Console.Write(" {0:D}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MatrixReader/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MatrixReader;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidMatrixInput()
+        {
+            //arrange
+            string filePath = @"D:\matrix.txt";
+            int[,] matrix;
+            int[,] check=new int [,] { { 1,2,3},{ 4,5,0},{ 0,0,0} };
+
+            //act
+            Program.MatrixReaderFromFile(filePath, out matrix);
+
+            //assert
+            Assert.AreEqual(check,matrix);
+
+        }
+    }
+}
